60e0aaf943ad50401112d657a53423ecc540af5d
Spitsyn Kirill <kaspicin2602@gmail.com>
2023-01-31 09:08:09 +0300

    models

--- src/test/java/in/reqres/testbase/TestBase.java
+++ src/test/java/in/reqres/testbase/TestBase.java
@@ -4,8 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
-    @BeforeAll
-    public static void setUp() {
-        Configuration.remote = System.getProperty("remoteUrl");
-    }
+//    @BeforeAll
+//    public static void setUp() {
+//        Configuration.remote = System.getProperty("remoteUrl");
+//    }
 }
